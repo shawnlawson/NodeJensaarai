6e5dd054defbe00b9b5bf156d634ca3b2af36602
Shawn Lawson <lawsos2@rpi.edu>
2018-09-28 16:14:14 -0400

    keyword add

--- public/lib/ace/mode-python.js
+++ public/lib/ace/mode-python.js
@@ -104,7 +104,7 @@ ace.define('ace/mode/python_highlight_rules', ['require', 'exports', 'module', '
 		'isFloat|isInt|isOP|isPython|isString|isToggle|style|isCustom|copy|eval|' +
 		'evalNorm|evalExpression|evalExport|create|par|pars()|expr|tdu|rand|' +
 		'parent|name|path|children|destroy|absTime|frame|seconds|outputConnectors|' +
-		'connect|disconnect|getGraph|resetPars|printPars|printFiles|printOut|')
+		'connect|disconnect|getGraph|resetPars|printPars|printFiles|printOut|pprint|')
 
     var keywordMapper = this.createKeywordMapper({
       'invalid.deprecated': 'debugger',
